a8886e4fa599f0a77a78d9cde67cdd7695b7e6d6
kiyo <142107103+notkiyo@users.noreply.github.com>
2024-09-16 23:20:36 -0700

    Add files via upload

---
+++ aiwaifu/apicode.py
@@ -0,0 +1,10 @@
+token = '' # add your discord token
+
+aiclient =('') # Add your c.ai API key here
+
+channel_id = '' # mode the line 247
+# if you dont knoe how to 
+ 
+# for discord watch this one = https://www.youtube.com/watch?v=dR9n1zmw-Go
+
+# for c.ai look this one = https://github.com/kramcat/CharacterAI
